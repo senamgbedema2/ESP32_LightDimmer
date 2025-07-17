2829d30f9f088d9133decc2618be01c11f3f7276
senamgbedema2 <senamgbedema2@gmail.com>
2025-07-17 22:16:58 +0000

    Update sketch_jul2a.ino

--- sketch_jul2a.ino
+++ sketch_jul2a.ino
@@ -1,17 +1,14 @@
 // Experiment 3: Reading a Potentiometer using ESP32
 // Define the analog pin
 const int potPin = 5;
-// GPI012 is input only
 
 void setup() {
     Serial.begin(115200);
     // Start Serial Monitor at 115200 baud rate
 }
-
 void loop() {
     int potValue = analogRead(potPin);
     // Read analog value (0-4095)
-
     Serial.print("Potentiometer Value: ");
     Serial.println(potValue);
     // Print the value
